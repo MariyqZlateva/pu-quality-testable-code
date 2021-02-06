e03d6f9983905e216385e4e8eb6678b196a93759
MariyaZlateva <mimitoooo@abv.bg>
2021-02-06 08:26:12 +0200

    Initial commit

---
+++ src/main/java/com/mariazlateva/puqualitytestablecode/PuQualityTestableCodeApplication.java
@@ -0,0 +1,13 @@
+package com.mariazlateva.puqualitytestablecode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuQualityTestableCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PuQualityTestableCodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mariazlateva/puqualitytestablecode/PuQualityTestableCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mariazlateva.puqualitytestablecode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuQualityTestableCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
